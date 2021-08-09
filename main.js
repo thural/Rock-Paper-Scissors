fb207c9e22c9c236cd3899179a880dfae371d4f4
Thural Musaibov <tural.musaibov@gmail.com>
2021-08-09 19:45:33 +0400

    add ui, refactor the code

--- main.js
+++ main.js
@@ -1,69 +1,71 @@
-    
-    const button = document.querySelector('button');
+let weapons=['rock', 'paper', 'scissors'];
+let playerSelection;
+let computerSelection;
+let userScore=0;
+let compScore=0;
 
-    let weapons=['rock', 'paper', 'scissors'];
-    let randomString;
-    let userChoice;
-    let compChoice;
-    let userScore=0;
-    let compScore=0;
+//creating dedicated funcions
 
-    function userPlays(){
-        userChoice = prompt("Choose Your Weapon");
-        userChoice=userChoice.toLowerCase();
-    }
+const compPlays=function(){
+    let randomString= Math.floor(Math.random() * weapons.length);
+    computerSelection=weapons[randomString];
+};
 
-    function compPlays(){
-        randomString= Math.floor(Math.random() * weapons.length);
-        compChoice=weapons[randomString];
-    }
+const logChoices=function(){
+    console.log("You choosen: " + playerSelection);
+    console.log("Computer choosen: " + computerSelection);
+};
 
-    function logChoices(){
-        console.log("Computer choosen: " + compChoice);
-        console.log("You choosen: " + userChoice);
-    }
+const listScenes=function(){
+    sceneOne=(playerSelection=="rock" && computerSelection=="scissors");
+    sceneTwo=(playerSelection=="paper" && computerSelection=="rock");
+    sceneThree=(playerSelection=="scissors" && computerSelection=="paper");
+};
 
-    function listScenes(){
-        sceneOne=(userChoice=="rock" && compChoice=="scissors");
-        sceneTwo=(userChoice=="paper" && compChoice=="rock");
-        sceneThree=(userChoice=="scissors" && compChoice=="paper");
-    }
-    
-    function calcScore(){
-        if (sceneOne || sceneTwo || sceneThree){
-            alert(userChoice + " beats " + compChoice + "\n \n You get +1");
-            userScore++;
-        } else if (userChoice===compChoice){
-            alert("Tie!");
-        } else{
-            alert(compChoice + " beats " +userChoice + "\n \n Computer gets +1");
-            compScore++;
+const calcScore=function(){
+    if (sceneOne || sceneTwo || sceneThree){
+        alert(playerSelection + " beats " + computerSelection + "\n \n You get +1");
+        userScore++;
+    } else if (playerSelection===computerSelection){
+        alert("Tie!");
+    } else{
+        alert(computerSelection + " beats " + playerSelection + "\n \n Computer gets +1");
+        compScore++;
+    };
+};
+
+const isGameOver=function(){
+    if (userScore==5){
+        alert("You Won!");
+        return compScore=0, userScore=0;
+        } else if(compScore==5){
+        alert("Computer Won!");
+        return compScore=0, userScore=0;
         }
-    }
+};
 
-    
+const playRound=function(weapon){
+    playerSelection=weapon;
+    compPlays();
+    logChoices();
+    listScenes();
+    calcScore();
+    results.innerHTML='Your score: '+userScore+' Computer score: '+compScore;
+    isGameOver();
+};
 
-    
+//Selecting HTML elements
+const buttons = document.querySelector('#buttons');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
 
-    button.onclick = function(){
+//Adding onclick functionality to selected elements
+rock.addEventListener('click', ()=> playRound("rock"));
+paper.addEventListener('click', ()=> playRound("paper"));
+scissors.addEventListener('click', ()=> playRound("scissors"));
 
-        for ( i = 1; i != Infinity; i++){
-                if (userScore==5){
-                alert("You Won!");
-                return compScore=0, userScore=0;
-                } else if(compScore==5){
-                 alert("Computer Won!");
-                 return compScore=0, userScore=0;
-                } else{
-                        userPlays();
-                        compPlays();
-                        logChoices();
-                        listScenes();
-                        calcScore();
-                        if( compScore!=5 && userScore!=5){
-                        alert("Your score is: " + userScore + "\n \n Computer score is: " + compScore);
-                        }
-                }
-            
-        }
-    }
+//Creating display area for game results
+const results=document.createElement('div');
+results.innerHTML='Your score: '+userScore+' Computer score: '+compScore;
+document.body.insertBefore(results, buttons);
